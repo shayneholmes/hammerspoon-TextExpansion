95432ae0137bf6cf8d780572c75f8b5692bae9b9
Shayne Holmes <shayneh@qualtrics.com>
2018-06-14 10:49:56 -0700

    Create file, populate with metadata

---
+++ init.lua
@@ -0,0 +1,21 @@
+--- === TextExpansion ===
+---
+--- Expand 'words' as you type, a la AutoHotKey's hotstrings.
+---
+--- TODO: Download link
+---
+--- Functionality inspired by AHK's [hotstrings](https://autohotkey.com/docs/Hotstrings.htm)
+
+local obj={}
+obj.__index = obj
+
+-- Dependencies
+
+-- Metadata
+obj.name = "TextExpansion"
+obj.version = "0.1"
+obj.author = "Shayne Holmes"
+obj.homepage = "https://github.com/shayneholmes/hammerspoon-TextExpansion"
+obj.license = "MIT - https://opensource.org/licenses/MIT"
+
+return obj
